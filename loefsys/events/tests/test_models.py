fc3d2c8fc480afe319188427e9f34c9b58cbed89
SypTitan <sybrentitan@gmail.com>
2024-11-05 22:16:13 +0100

    Revert "Testing with a false test"
    This reverts commit a0fd03d7d7e09c572c0820d9df8a7d8faed14f66.

--- loefsys/events/tests/test_models.py
+++ loefsys/events/tests/test_models.py
@@ -15,7 +15,6 @@ class EventTestCase(TestCase):
 class MandatoryRegistrationDetailsTestCase(TestCase):
     def test_create(self):
         details = G(MandatoryRegistrationDetails)
-        self.assertFalse(True)
         self.assertIsNotNone(details)
         self.assertIsNotNone(details.pk)
 
